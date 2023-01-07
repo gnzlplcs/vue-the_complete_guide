48de655ceaf7e6bb1a9fd2b27d5f073f0e2bcbb7
gnzlplcs <palaciosgm@outlook.com>
2023-01-07 14:59:56 -0500

    :construction: create and connect Vue app instances

---
+++ basics-01-starting-code/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    // always returns an object
+    return {
+      courseGial: 'Finish the Vue course'
+    }
+  },
+  methods:
+});
+
+app.mount("#user-goal");
